a9f282528f3f5ab013b657c93947aa30b4129077
Muge-owo <3516169371@qq.com>
2026-07-28 21:05:24 +0800

    完成05-strings练习

--- main.c
+++ main.c
@@ -2,21 +2,20 @@
 
 int main(int argc, const char *argv[])
 {
-    int arr[5] = {0};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    
-    for(int i = 0; i < size; i++){
-        if(scanf("%d", &arr[i]) != 1)
-            return -1;
+    char arr[100];
+    int len = 0;
+
+    if(fgets(arr, 99, stdin) == NULL)
+        return -1;
+    for(int i = 0; i < 99; i++){
+        if(arr[i] == '\n'){
+            arr[i] = '\0';
+            break;
+        }
+        len++;
     }
 
-    int Max = arr[0];
-    for(int i = 1; i < size; i++){
-        if(Max < arr[i])
-            Max = arr[i];
-    }
-
-    printf("%d\n", Max);
+    printf("%d\n", len);
 
     return 0;
 }
