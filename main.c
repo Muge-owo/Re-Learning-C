987b8f2ae6049672eeec54c6375e4dee1571bc79
Muge-owo <3516169371@qq.com>
2026-07-28 20:27:24 +0800

    完成04-arrays练习

--- main.c
+++ main.c
@@ -2,12 +2,21 @@
 
 int main(int argc, const char *argv[])
 {
-    int iWidth = 0;
-    int iHeight = 0;
-    if(scanf("%d", &iWidth) != 1)
-        return -1;
-    if(scanf("%d", &iHeight) != 1)
-        return -1;
+    int arr[5] = {0};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    for(int i = 0; i < size; i++){
+        if(scanf("%d", &arr[i]) != 1)
+            return -1;
+    }
 
-    printf("%d\n", iWidth * iHeight);
+    int Max = arr[0];
+    for(int i = 1; i < size; i++){
+        if(Max < arr[i])
+            Max = arr[i];
+    }
+
+    printf("%d\n", Max);
+
+    return 0;
 }
