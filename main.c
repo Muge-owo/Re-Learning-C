328b7869e4be75ccb70f00ab1f3e8f9c86905162
Muge-owo <3516169371@qq.com>
2026-07-28 19:46:13 +0800

    完成02variables-and-types练习

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, C!\n");
-    return 0;
+int numadd(int a, int b)
+{
+    return a + b;
+} 
+
+int main(int argc, const char *argv[])
+{
+    int ia = 0;
+    int ib = 0;
+    if(scanf("%d", &ia) != 1)
+        return -1;
+    if(scanf("%d", &ib) != 1)
+        return -1;
+
+    printf("%d\n", numadd(ia, ib));
 }
