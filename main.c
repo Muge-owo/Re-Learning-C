a0aa2bd620f91d6675c1ebb39c5a3685047cccfa
Muge-owo <3516169371@qq.com>
2026-07-29 08:47:31 +0800

    完成06-conditionals练习

--- main.c
+++ main.c
@@ -2,20 +2,17 @@
 
 int main(int argc, const char *argv[])
 {
-    char arr[100];
-    int len = 0;
-
-    if(fgets(arr, 99, stdin) == NULL)
+    int num;
+    if(scanf("%d", &num) != 1)
         return -1;
-    for(int i = 0; i < 99; i++){
-        if(arr[i] == '\n'){
-            arr[i] = '\0';
-            break;
-        }
-        len++;
-    }
 
-    printf("%d\n", len);
+    if(num % 3 == 0 && num % 5 == 0)
+        printf("FizzBuzz\n");
+    else if(num % 3 == 0)
+        printf("Fizz\n");
+    else if(num % 5 == 0)
+        printf("Buzz\n");
+    
 
     return 0;
 }
