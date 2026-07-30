1d4470e014ae8e4a88c8e45a1805881ace2507ac
Muge-owo <3516169371@qq.com>
2026-07-30 09:18:23 +0800

    完成13-preprocessor练习

--- main.c
+++ main.c
@@ -1,36 +1,16 @@
 #include <stdio.h>
 
+#define CUBE(x) (x)*(x)*(x)
+
 int main(int argc, const char *argv[])
 {
-    int n;
-    if(scanf("%d", &n) != 1)
+    int x;
+    if(scanf("%d", &x) != 1){
+        fprintf(stderr, "__%d__\n", __LINE__);
         return -1;
-
-    FILE *fp = fopen("/tmp/clines.txt", "w");
-    if(fp == NULL)
-        return -2;
-
-    char str[1024];
-    int size = sizeof(str) / sizeof(str[0]);
-    for(int i = 0; i < n; i++){
-        if(fgets(str, size, stdin) == NULL)
-            return -3;
-
-        fprintf(fp , "%s", str);
     }
-    fclose(fp);
 
-    FILE *in = fopen("/tmp/clines.txt", "r");
-    if(in == NULL)
-        return -4;
-
-    int line = 0;
-
-    while(fgets(str, size, in) != NULL)
-        line++;
-
-    printf("lines: %d\n", line);
-    fclose(in);
+    printf("%d cubed is %d\n", x, CUBE(x));
 
     return 0;
 }
