e37a6bf2f9d567bfd53414caf807e60209937942
Muge-owo <3516169371@qq.com>
2026-07-30 09:04:38 +0800

    完成12-file-io练习(2)

--- main.c
+++ main.c
@@ -6,28 +6,26 @@ int main(int argc, const char *argv[])
     if(scanf("%d", &n) != 1)
         return -1;
 
-    FILE* out = fopen("/tmp/clines.txt", "w");
-    if(out == NULL)
-        return -1;
-    char str[1024];
-    int size = sizeof(str) / sizeof(str[0]);
-
-    fprintf(out, "%d", n);
-    for(int i = 0; i < n; i++){
-        if(fgets(str, size, stdin) == NULL)
-            return -2;
-        // if(scanf("%s", str) != 1){
-        //     return -2;
-        // }
-        fprintf(out, "%s", str);
-    }
-    fclose(out);
-
-    FILE* in = fopen("/tmp/clines.txt", "r");
-    if(in == NULL)
+    FILE *fp = fopen("/tmp/clines.txt", "w");
+    if(fp == NULL)
         return -2;
 
+    char str[1024];
+    int size = sizeof(str) / sizeof(str[0]);
+    for(int i = 0; i < n; i++){
+        if(fgets(str, size, stdin) == NULL)
+            return -3;
+
+        fprintf(fp , "%s", str);
+    }
+    fclose(fp);
+
+    FILE *in = fopen("/tmp/clines.txt", "r");
+    if(in == NULL)
+        return -4;
+
     int line = 0;
+
     while(fgets(str, size, in) != NULL)
         line++;
 
